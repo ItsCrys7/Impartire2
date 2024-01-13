0d68d1cecf474a1dfc56b109b9a4ec40cd7d3d31
H3RØ <146414762+ItsCrys7@users.noreply.github.com>
2024-01-13 07:45:02 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Impartire2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (b != 0 && a % b == 00)
+            {
+                c = a / b;
+
+                Console.WriteLine(c);
+            }
+            else
+            {
+                Console.WriteLine("Impartirea nu se poate efectuata");
+            }
+
+        }
+    }
+}
